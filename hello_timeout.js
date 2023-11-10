536f1e1bef0c51510cba0e48a959b67399ca02bd
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-09 20:25:01 -0500

    first commit

---
+++ hello_timeout.js
@@ -0,0 +1,15 @@
+const words1 = "4311o";
+const words2 = "th3r3";
+const words3 = "w0r1d";
+
+setTimeout(() => {
+  console.log(words1);
+}, 1000);
+
+setTimeout(() => {
+  console.log(words2);
+}, 2000);
+
+setTimeout(() => {
+  console.log(words3);
+}, 3000);
